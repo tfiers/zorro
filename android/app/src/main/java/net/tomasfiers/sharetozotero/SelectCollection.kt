cb1ed81580b2ade0c78c5be0419c3303d524dbb9
Tomas Fiers <tomas.fiers@gmail.com>
2019-11-08 23:34:06 +0100

    Receive text and get in share menu

---
+++ android/app/src/main/java/net/tomasfiers/sharetozotero/SelectCollection.kt
@@ -0,0 +1,20 @@
+package net.tomasfiers.sharetozotero
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SelectCollection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_collection)
+        val textfield = findViewById<TextView>(R.id.textfield)
+        if (intent?.action == Intent.ACTION_SEND) {
+            textfield.text = intent.getStringExtra(Intent.EXTRA_TEXT)
+        } else {
+            // Handle being started from home screen.
+        }
+    }
+}
