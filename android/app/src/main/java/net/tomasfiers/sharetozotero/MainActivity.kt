d33dd0c4450363a963fa435687f8ddbe7e18a921
Tomas Fiers <tomas.fiers@gmail.com>
2019-11-09 01:54:06 +0100

    Parse JSON further

--- android/app/src/main/java/net/tomasfiers/sharetozotero/MainActivity.kt
+++ android/app/src/main/java/net/tomasfiers/sharetozotero/MainActivity.kt
@@ -5,25 +5,27 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.textView)
         val queue = Volley.newRequestQueue(this)
         val request = ZoteroAPIRequest("/collections",
-            Response.Listener { response ->
-                textView.text = response[0].toString()
-            },
+            Response.Listener { displayCollections(it) },
             Response.ErrorListener {
                 textView.text = "Downloading Zotero collection failed"
             })
         queue.add(request)
+    }
+
+    private fun displayCollections(collections: JSONArray) {
+        textView.text = (collections[0] as JSONObject)["data"].toString()
     }
 }
 
