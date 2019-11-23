200f98818c303fabfe2afcf2b1bc56615bdd3941
Tomas Fiers <tomas.fiers@gmail.com>
2019-11-23 17:40:39 +0100

    Refactor and rename ViewHolder

--- android/app/src/main/java/net/tomasfiers/zoro/ui/RecyclerViewAdapter.kt
+++ android/app/src/main/java/net/tomasfiers/zoro/ui/RecyclerViewAdapter.kt
@@ -9,31 +9,39 @@ import net.tomasfiers.zoro.data.TreeItem
 import net.tomasfiers.zoro.databinding.TreeItemBinding
 
 class ListItemDiffCallback : DiffUtil.ItemCallback<TreeItem>() {
-    override fun areItemsTheSame(oldItem: TreeItem, newItem: TreeItem) = oldItem.id == newItem.id
+    override fun areItemsTheSame(oldItem: TreeItem, newItem: TreeItem) =
+        oldItem.id == newItem.id
+
     override fun areContentsTheSame(oldItem: TreeItem, newItem: TreeItem) =
         oldItem.name == newItem.name
 }
 
 // One container in the RecyclerView list.
-class MyViewHolder(private val binding: TreeItemBinding) : RecyclerView.ViewHolder(binding.root) {
+class TreeItemViewHolder private constructor(private val binding: TreeItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
     fun bind(treeItem: TreeItem) {
         binding.item = treeItem
         // (A recommended slight speed optimization:)
         binding.executePendingBindings()
     }
+
+    companion object {
+        fun from(parent: ViewGroup): TreeItemViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = TreeItemBinding.inflate(layoutInflater, parent, false)
+            return TreeItemViewHolder(binding)
+        }
+    }
 }
 
-class RecyclerViewAdapter : ListAdapter<TreeItem, MyViewHolder>(ListItemDiffCallback()) {
+class RecyclerViewAdapter : ListAdapter<TreeItem, TreeItemViewHolder>(ListItemDiffCallback()) {
 
     // Called when the RecyclerView requests a new container to add to the list.
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = TreeItemBinding.inflate(layoutInflater, parent, false)
-        return MyViewHolder(binding)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        TreeItemViewHolder.from(parent)
 
     // Called when the RecyclerView wants to fill a container with a concrete item.
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: TreeItemViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 }
