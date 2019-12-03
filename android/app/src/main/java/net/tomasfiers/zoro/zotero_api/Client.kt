d0427829282c7b3466c043e57f77df09c05251e6
Tomas Fiers <tomas.fiers@gmail.com>
2019-12-03 22:11:46 +0100

    Insert headers on all requests using OkHttp

--- android/app/src/main/java/net/tomasfiers/zoro/zotero_api/Client.kt
+++ android/app/src/main/java/net/tomasfiers/zoro/zotero_api/Client.kt
@@ -1,24 +1,32 @@
 package net.tomasfiers.zoro.zotero_api
 
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
-private val httpClientBuilder = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(jsonParserBuilder))
+private val zoteroHTTPClient = OkHttpClient.Builder()
+    .addInterceptor { chain ->
+        chain.proceed(
+            chain.request().newBuilder()
+                .addHeader("Zotero-API-Version", "3")
+                .addHeader("Zotero-API-Key", ZOTERO_API_KEY)
+                .build()
+        )
+    }
+    .build()
+
+private val zoteroAPIClientCreator = Retrofit.Builder()
+    .client(zoteroHTTPClient)
     .baseUrl("https://api.zotero.org/users/4670453/")
+    .addConverterFactory(MoshiConverterFactory.create(jsonParserBuilder))
     .build()
 
 const val MAX_ITEMS_PER_RESPONSE = 100
 
 interface ZoteroAPIClient {
-    @Headers(
-        "Zotero-API-Version: 3",
-        "Zotero-API-Key: $ZOTERO_API_KEY"
-    )
     @GET("collections")
     suspend fun getSomeCollections(
         @Query("start") startIndex: Int = 100,
@@ -29,5 +37,5 @@ interface ZoteroAPIClient {
 
 // "lazy" computes value only on first access (client creation is expensive).
 val zoteroAPIClient: ZoteroAPIClient by lazy {
-    httpClientBuilder.create(ZoteroAPIClient::class.java)
+    zoteroAPIClientCreator.create(ZoteroAPIClient::class.java)
 }
