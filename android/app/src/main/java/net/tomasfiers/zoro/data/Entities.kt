37a03fa0e5c5a0175cb790045905b7686b6dc551
Tomas Fiers <tomas.fiers@gmail.com>
2019-12-07 16:33:55 +0100

    Define relationships between Item/Collection/Creator

--- android/app/src/main/java/net/tomasfiers/zoro/data/Entities.kt
+++ android/app/src/main/java/net/tomasfiers/zoro/data/Entities.kt
@@ -1,40 +1,78 @@
 package net.tomasfiers.zoro.data
 
 import androidx.room.Entity
+import androidx.room.Junction
 import androidx.room.PrimaryKey
+import androidx.room.Relation
 import org.threeten.bp.OffsetDateTime
 
+// ID's will probably not overlap between Collections and Items. An ID has 8 alphanumeric
+// characters. 8^36 unique IDs = 2821 billion = 10^12 = 1/10 of cells in human body.
 interface TreeItem {
     val id: String
     var version: Long
     var name: String
 }
 
+// We need separately named "id" columns to associate Collection and Item.
 @Entity
 data class Collection(
     @PrimaryKey
-    override val id: String,
+    val collectionId: String,
     override var version: Long,
     override var name: String,
-    var parentId: String?
-) : TreeItem
+    var parentId: String?,
+    @Relation(
+        parentColumn = "collectionId",
+        entityColumn = "itemId",
+        associateBy = Junction(ItemCollectionAssociation::class)
+    )
+    val items: List<Item>
+) : TreeItem {
+    override val id: String
+        get() = collectionId
+}
 
 @Entity
 data class Item(
     @PrimaryKey
-    override val id: String,
+    val itemId: String,
     override var version: Long,
     override var name: String,
     var type: ItemType,
     var dateAdded: OffsetDateTime,
-    var dateModified: OffsetDateTime
-) : TreeItem, ItemMixin()
+    var dateModified: OffsetDateTime,
+    @Relation(
+        parentColumn = "itemId",
+        entityColumn = "collectionId",
+        associateBy = Junction(ItemCollectionAssociation::class)
+    )
+    val collections: List<Collection>,
+
+    @Relation(
+        parentColumn = "itemId",
+        entityColumn = "creatorId"
+    )
+    val creators: List<Creator>,
+    val relatedItemIds: List<String>
+
+) : TreeItem, ItemMixin() {
+    override val id: String
+        get() = itemId
+}
 
 @Entity
 data class Creator(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val creatorId: Int,
     var firstName: String?,
     var lastName: String?,
-    var name: String?
+    var name: String?,
+    var type: CreatorType
+)
+
+@Entity(primaryKeys = ["collectionId", "itemId"])
+data class ItemCollectionAssociation(
+    val collectionId: String,
+    val itemId: String
 )
