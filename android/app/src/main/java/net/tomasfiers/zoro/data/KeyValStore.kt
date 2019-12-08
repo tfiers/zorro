634566af64bd03e7ed8e2ca607441933c6e5fb70
Tomas Fiers <tomas.fiers@gmail.com>
2019-12-08 18:35:41 +0100

    Nicely compromised refactor of KeyValStore

--- android/app/src/main/java/net/tomasfiers/zoro/data/KeyValStore.kt
+++ android/app/src/main/java/net/tomasfiers/zoro/data/KeyValStore.kt
@@ -11,22 +11,37 @@ class KeyValStore(application: Application) {
         "net.tomasfiers.zoro.MyKeyValStore", Context.MODE_PRIVATE
     )
 
-    private enum class Keys {
-        LOCAL_LIBRARY_VERSION,
-        LOCAL_SCHEMA_ETAG
+
+    var localLibraryVersion
+        get() = get(Key.LOCAL_LIBRARY_VERSION) as Int
+        set(value) = set(Key.LOCAL_LIBRARY_VERSION, value)
+
+    var localSchemaETag
+        get() = get(Key.LOCAL_SCHEMA_ETAG) as String?
+        set(value) = set(Key.LOCAL_SCHEMA_ETAG, value)
+
+
+    private enum class Key(val valueType: ValueType, val defaultValue: Any?) {
+        LOCAL_LIBRARY_VERSION(ValueType.INT, INITIAL_LOCAL_LIBRARY_VERSION),
+        LOCAL_SCHEMA_ETAG(ValueType.STRING, null)
     }
 
-    var localLibraryVersion: Int
-        get() = store.getInt(Keys.LOCAL_LIBRARY_VERSION.name, INITIAL_LOCAL_LIBRARY_VERSION)
-        set(value) = with(store.edit()) {
-            putInt(Keys.LOCAL_LIBRARY_VERSION.name, value)
-            apply()
-        }
+    private enum class ValueType { STRING, INT }
 
-    var localSchemaETag: String?
-        get() = store.getString(Keys.LOCAL_SCHEMA_ETAG.name, null)
-        set(value) = with(store.edit()) {
-            putString(Keys.LOCAL_SCHEMA_ETAG.name, value)
+    private fun get(key: Key): Any? {
+        return when (key.valueType) {
+            ValueType.STRING -> store.getString(key.name, key.defaultValue as String?)
+            ValueType.INT -> store.getInt(key.name, key.defaultValue as Int)
+        }
+    }
+
+    private fun set(key: Key, value: Any?) {
+        with(store.edit()) {
+            when (key.valueType) {
+                ValueType.STRING -> putString(key.name, value as String?)
+                ValueType.INT -> putInt(key.name, value as Int)
+            }
             apply()
         }
+    }
 }
