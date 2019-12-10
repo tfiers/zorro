82eb06343d81aab0844bbde09926808ba4e2432d
Tomas Fiers <tomas.fiers@gmail.com>
2019-12-10 21:37:26 +0100

    More accurate syncStatus texts

--- android/app/src/main/java/net/tomasfiers/zoro/sync/SyncCollections.kt
+++ android/app/src/main/java/net/tomasfiers/zoro/sync/SyncCollections.kt
@@ -7,29 +7,33 @@ import net.tomasfiers.zoro.zotero_api.MAX_ITEMS_PER_RESPONSE
 import net.tomasfiers.zoro.zotero_api.remoteLibraryVersion
 
 suspend fun DataRepo.syncCollections(): Int? {
-    syncStatus.value = "Updating collections…"
+    syncStatus.value = "Checking for updated collections…"
     val collectionVersionsResponse = zoteroAPIClient.getCollectionVersions(
         sinceLibraryVersion = getValue(Key.LOCAL_LIBRARY_VERSION)
     )
     val remoteLibVersionAtStartSync = collectionVersionsResponse.remoteLibraryVersion
     val collectionIds = collectionVersionsResponse.body()?.keys ?: emptyList<String>()
-    syncStatus.value = "Downloading ${collectionIds.size} collections…"
-    showProgressBar.value = true
-    downloadProgress.value = 0f
-    var currentCollectionNr = 1
-    collectionIds
-        .chunked(MAX_ITEMS_PER_RESPONSE)
-        .forEach { someCollectionIds ->
-            val jsonCollectionsResponse =
-                zoteroAPIClient.getCollections(someCollectionIds.joinToString(","))
-            if (jsonCollectionsResponse.remoteLibraryVersion != remoteLibVersionAtStartSync) {
-                throw RemoteLibraryUpdatedSignal()
+    val numCollectionsToDownload = collectionIds.size
+    if (numCollectionsToDownload > 0) {
+        syncStatus.value = "Downloading ${collectionIds.size} collections…"
+        showProgressBar.value = true
+        downloadProgress.value = 0f
+        var currentCollectionNr = 1
+        collectionIds
+            .chunked(MAX_ITEMS_PER_RESPONSE)
+            .forEach { someCollectionIds ->
+                val jsonCollectionsResponse =
+                    zoteroAPIClient.getCollections(someCollectionIds.joinToString(","))
+                if (jsonCollectionsResponse.remoteLibraryVersion != remoteLibVersionAtStartSync) {
+                    throw RemoteLibraryUpdatedSignal()
+                }
+                jsonCollectionsResponse.body()?.forEach {
+                    database.collection.insert(it.asDomainModel())
+                    downloadProgress.value =
+                        (currentCollectionNr++).toFloat() / numCollectionsToDownload
+                }
             }
-            jsonCollectionsResponse.body()?.forEach {
-                database.collection.insert(it.asDomainModel())
-                downloadProgress.value = (currentCollectionNr++).toFloat() / collectionIds.size
-            }
-        }
-    showProgressBar.value = false
+        showProgressBar.value = false
+    }
     return remoteLibVersionAtStartSync
 }
--- android/app/src/main/java/net/tomasfiers/zoro/sync/SyncItems.kt
+++ android/app/src/main/java/net/tomasfiers/zoro/sync/SyncItems.kt
@@ -7,26 +7,29 @@ import net.tomasfiers.zoro.zotero_api.MAX_ITEMS_PER_RESPONSE
 import net.tomasfiers.zoro.zotero_api.remoteLibraryVersion
 
 suspend fun DataRepo.syncItems(remoteLibVersionAtStartSync: Int?) {
-    syncStatus.value = "Updating items…"
+    syncStatus.value = "Checking for updated items…"
     val itemVersions = zoteroAPIClient.getItemVersions(
         sinceLibraryVersion = getValue(Key.LOCAL_LIBRARY_VERSION)
     )
     val itemIds = itemVersions.keys
-    syncStatus.value = "Downloading ${itemIds.size} items…"
-    showProgressBar.value = true
-    downloadProgress.value = 0f
-    var currentItemNr = 1
-    itemIds
-        .chunked(MAX_ITEMS_PER_RESPONSE)
-        .forEach { someItemIds ->
-            val response = zoteroAPIClient.getItems(someItemIds.joinToString(","))
-            if (response.remoteLibraryVersion != remoteLibVersionAtStartSync) {
-                throw RemoteLibraryUpdatedSignal()
+    val numItemsToDownload = itemIds.size
+    if (numItemsToDownload > 0) {
+        syncStatus.value = "Downloading ${itemIds.size} items…"
+        showProgressBar.value = true
+        downloadProgress.value = 0f
+        var currentItemNr = 1
+        itemIds
+            .chunked(MAX_ITEMS_PER_RESPONSE)
+            .forEach { someItemIds ->
+                val response = zoteroAPIClient.getItems(someItemIds.joinToString(","))
+                if (response.remoteLibraryVersion != remoteLibVersionAtStartSync) {
+                    throw RemoteLibraryUpdatedSignal()
+                }
+                response.body()?.forEach { itemJson ->
+                    database.item.insert(itemJson.asDomainModel())
+                    downloadProgress.value = (currentItemNr++).toFloat() / numItemsToDownload
+                }
             }
-            response.body()?.forEach { itemJson ->
-                database.item.insert(itemJson.asDomainModel())
-                downloadProgress.value = (currentItemNr++).toFloat() / itemIds.size
-            }
-        }
-    showProgressBar.value = false
+        showProgressBar.value = false
+    }
 }
