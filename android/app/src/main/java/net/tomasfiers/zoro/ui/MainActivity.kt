7495f0a9fde21ef415aff83ffac8cefe9befad38
Tomas Fiers <tomas.fiers@gmail.com>
2019-12-10 20:43:25 +0100

    Extract methods from MainActivity.onCreate

--- android/app/src/main/java/net/tomasfiers/zoro/ui/MainActivity.kt
+++ android/app/src/main/java/net/tomasfiers/zoro/ui/MainActivity.kt
@@ -13,39 +13,31 @@ import kotlinx.coroutines.launch
 import net.tomasfiers.zoro.BuildConfig
 import net.tomasfiers.zoro.R
 import net.tomasfiers.zoro.ZoroApplication
+import net.tomasfiers.zoro.data.DataRepo
 import net.tomasfiers.zoro.databinding.MainActivityBinding
 import net.tomasfiers.zoro.sync.syncLibrary
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: MainActivityBinding
+    lateinit var dataRepo: DataRepo
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
-
-        // Set toolbar to act as activity's action bar
-        setSupportActionBar(binding.toolbar)
-
-        // Connect toolbar to drawerLayout
-        actionBarDrawerToggle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayout,
-            binding.toolbar,
-            R.string.close_drawer,
-            R.string.open_drawer
-        )
-        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
-
+        dataRepo = (application as ZoroApplication).dataRepo
+        setupToolbar()
+        observeSyncErrors()
+        lifecycleScope.launch { dataRepo.syncLibrary() }
         if (BuildConfig.DEBUG) {
             // Keep screen on for development ease.
             window.addFlags(FLAG_KEEP_SCREEN_ON)
         }
+    }
 
+    private fun observeSyncErrors() {
         var snackbar: Snackbar? = null
-        val dataRepo = (application as ZoroApplication).dataRepo
         dataRepo.syncError.observe(this, Observer { error ->
             when (error) {
                 null -> snackbar?.dismiss()
@@ -60,7 +52,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
-        lifecycleScope.launch { dataRepo.syncLibrary() }
+    }
+
+    private fun setupToolbar() {
+        // Set toolbar to act as activity's action bar
+        setSupportActionBar(binding.toolbar)
+        // Connect toolbar to drawerLayout
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.close_drawer,
+            R.string.open_drawer
+        )
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
