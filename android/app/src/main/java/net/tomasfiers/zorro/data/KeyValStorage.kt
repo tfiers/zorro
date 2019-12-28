96b78f2190c93b592d3c48d4718912e4336cc4f9
Tomas Fiers <tomas.fiers@gmail.com>
2019-12-28 20:32:39 +0100

    Developer mode switch now toggles db value

--- android/app/src/main/java/net/tomasfiers/zorro/data/KeyValStorage.kt
+++ android/app/src/main/java/net/tomasfiers/zorro/data/KeyValStorage.kt
@@ -6,22 +6,23 @@ const val INITIAL_LOCAL_LIBRARY_VERSION = 0
 
 enum class Key(val dataType: DataType, val defaultValue: Any?) {
     LOCAL_LIBRARY_VERSION(DataType.INT, INITIAL_LOCAL_LIBRARY_VERSION),
-    LOCAL_SCHEMA_ETAG(DataType.STRING, null);
+    LOCAL_SCHEMA_ETAG(DataType.STRING, null),
+    DEVELOPER_MODE(DataType.BOOLEAN, false);
 
-    enum class DataType { STRING, INT }
+    enum class DataType { STRING, INT, BOOLEAN }
 }
-
-// Suspend properties (getters and setters) don't exist in Kotlin.
-// So no `keyValStore.localLibraryVersion = x` unfortunately :'(
-
-suspend inline fun <reified T> DataRepo.getValue(key: Key): T =
-    when (val value = database.keyValPair.get(key.name)?.value) {
-        null -> key.defaultValue as T
-        else -> when (key.dataType) {
-            Key.DataType.STRING -> value as T
-            Key.DataType.INT -> value.toInt() as T
-        }
-    }
 
 suspend fun DataRepo.setValue(key: Key, value: Any?) =
     database.keyValPair.insert(KeyValPair(key.name, value.toString()))
+
+suspend inline fun <reified T> DataRepo.getValue(key: Key): T {
+    val stringValue = database.keyValPair.get(key.name)?.value
+    return when (stringValue) {
+        null -> key.defaultValue
+        else -> when (key.dataType) {
+            Key.DataType.STRING -> stringValue
+            Key.DataType.INT -> stringValue.toInt()
+            Key.DataType.BOOLEAN -> stringValue.toBoolean()
+        }
+    } as T
+}
--- android/app/src/main/java/net/tomasfiers/zorro/ui/browsing/DrawerMenuVM.kt
+++ android/app/src/main/java/net/tomasfiers/zorro/ui/browsing/DrawerMenuVM.kt
@@ -1,14 +1,32 @@
 package net.tomasfiers.zorro.ui.browsing
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import net.tomasfiers.zorro.data.DataRepo
+import net.tomasfiers.zorro.data.Key
 import net.tomasfiers.zorro.data.clearLocalData
+import net.tomasfiers.zorro.data.getValue
+import net.tomasfiers.zorro.data.setValue
 
 class DrawerMenuViewModel(private val dataRepo: DataRepo) : ViewModel() {
 
     val lastSyncText = dataRepo.lastSyncText
+    val developerMode = MutableLiveData(false)
+
+    init {
+        viewModelScope.launch {
+            developerMode.value = dataRepo.getValue(Key.DEVELOPER_MODE)
+        }
+    }
+
+    fun toggleDeveloperMode() {
+        developerMode.value = !developerMode.value!!
+        viewModelScope.launch {
+            dataRepo.setValue(Key.DEVELOPER_MODE, developerMode.value)
+        }
+    }
 
     fun clearLocalData() = viewModelScope.launch {
         dataRepo.clearLocalData()
